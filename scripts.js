9c27a448f7edef3b20ce33711bce3f6b4cf67099
godigitinsurance <59437973+godigitinsurance@users.noreply.github.com>
2021-09-15 19:10:44 +0530

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,13 @@
+$("#customerName").text("Shiv Kumar");
+$("#policyNumber").text("D037673294 / 14062021");
+$("#startDate").text("24-Jun-2021");
+$("#endDate").text("23-Jun-2022");
+
+
+$("#type").text("Two Wheeler");
+$("#registrationNumber").text("PB08CZ5618");
+$("#make").text("BAJAJ");
+$("#model").text("DISCOVER M");
+$("#vehicalSubType").text("DRUM BRAKE");
+$("#engineNo").text("PAZRFK99083");
+$("#chassis").text("MD2A57BZXFRK78669");
